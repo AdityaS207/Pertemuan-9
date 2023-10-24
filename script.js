1c53f40759c6cc9d86e1da869ca12e38f4309efb
ADT <adityasitumorang12345@gmail.com>
2023-10-24 09:17:52 +0700

    Make calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+const {createApp, ref, computed} = Vue
+
+createApp({
+  data() {
+    const input1 = ref('');
+    const input2 = ref('');
+    const hasil_jumlah = computed(() => parseFloat(input1.value) + parseFloat(input2.value));
+    const hasil_kurang = computed(() => parseFloat(input1.value) - parseFloat(input2.value));
+    const hasil_kali = computed(() => parseFloat(input1.value) * parseFloat(input2.value));
+    const hasil_bagi = computed(() => parseFloat(input1.value) / parseFloat(input2.value));
+    return {
+        input1,
+        input2,
+        hasil: 0,
+        hasil_jumlah,
+        hasil_kurang,
+        hasil_kali,
+        hasil_bagi,
+    }
+  }
+}).mount('#app')
